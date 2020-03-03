289578e71df9c9d5fd652697a40ad6f23acf9cb1
Yukito <zan77739www@gmail.com>
2020-03-04 00:44:37 +0900

    create class component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
-function App() {
-  return (
-    <h1>
-      Hello, World!
-    </h1>
-  );
+// function App() {
+//   return (
+//     <h1>
+//       Hello, World!
+//     </h1>
+//   );
+// }
+
+const App = () => {
+return <div>Hi!Catsss</div>
+}
+
+const Cat = () => {
+  return <div>Cat!</div>
 }
 
 export default App;
