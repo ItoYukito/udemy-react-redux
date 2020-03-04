566e00887b45560116db441ab53f31fdfb9da636
Yukito <zan77739www@gmail.com>
2020-03-04 21:36:52 +0900

    actions practice

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+export const INCREMENT = 'INCREMENT'
+export const DECRIMENT = 'DECRIMENT'
+
+export const increment = () => {
+    return{
+        type: 'INCREMENT'
+    }
+}
+export const decriment = () => {
+    return{
+        type: 'DECRIMENT'
+    }
+}
