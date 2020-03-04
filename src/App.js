874b21a7f918e13570f1892909d766e746e26454
Yukito <zan77739www@gmail.com>
2020-03-04 21:10:38 +0900

    state practice

--- src/App.js
+++ src/App.js
@@ -1,34 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component} from 'react';
 
-const App = () => {
-  const profiles = [
-    {name: "taro",age: 10},
-    {name:"hanako",age: 222},
-    {name:"noname"}
-  ]
-  return (
-  <div>
-    {
-      profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index}/>
-      })
-    }
-    </div>
-  )
-}
+const App = () => (<Counter></Counter> )
 
-const User = (props) => {
-  return <div>I am {props.name}, and {props.age}</div>
-}
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { count: 0}
+  }
 
-// User.defaultProps = {
-//   age: 1
-// }
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1})
+  }
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+  hadleMinusButton = () => {
+    this.setState({count: this.state.count - 1})
+  }
+
+
+  render() {
+    return(
+      <React.Fragment>
+        <div>counter: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.hadleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
